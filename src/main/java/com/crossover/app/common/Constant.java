8cf5d34ed9d89deec09a0664e5480b6f4b1094be
paragdhoble <41855589+paragdhoble@users.noreply.github.com>
2018-08-12 18:55:00 +0530

    Update Constant.java

--- src/main/java/com/crossover/app/common/Constant.java
+++ src/main/java/com/crossover/app/common/Constant.java
@@ -9,11 +9,11 @@ public class Constant {
     public static final String URL = "https://accounts.google.com/ServiceLogin?";
     public static final String Sender = "001test.automation@gmail.com";
     public static final String Recipient = "002test.automation@gmail.com";
-    public static final String Password = "Tech@8092";
-    public static final String Email_Body = "Dear Mail Crawler,"
+    public static final String Password = "";
+    public static final String Email_Body = "Dear mail Crawler,"
             + "\n\n No spam to my email, please!" ;
 
-    public static final String  EmailUserName = "parag.dhoble@globallogic.com";
-    public static final String  EmailPassword = "Tech8092";
+    public static final String  EmailUserName = "parag.dhoble@gmail.com";
+    public static final String  EmailPassword = "";
 
 }
